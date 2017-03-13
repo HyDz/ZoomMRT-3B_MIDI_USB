fef5f3d9c9c57ab0a303e175d5e6debe38dd8aa0
HyDz <jean@toutourien.org>
2017-03-13 17:39:26 +0100

    Update inputs.h

--- main/inputs.h
+++ main/inputs.h
@@ -7,82 +7,82 @@
  * Declare Digitals Inputs
  */ 
  
-int SongSwitch = 2;
+const int SongSwitch = 2;
 int valSongSwitch = 0;
 int lastSongSwitch = 0;
 
-int PatternSwitch = 3;
+const int PatternSwitch = 3;
 int valPatternSwitch = 0;
 int lastPatternSwitch = 0;
 
-int KitSwitch = 4;
+const int KitSwitch = 4;
 int valKitSwitch = 0;
 int lastKitSwitch = 0;
 
-int InsertSwitch = 5;
+const int InsertSwitch = 5;
 int valInsertSwitch =0;
 int lastInsertSwitch = 0;
 
-int DeleteSwitch = 6;
+const int DeleteSwitch = 6;
 int valDeleteSwitch = 0;
 int lastDeleteSwitch = 0;
 
-int MinusSwitch = 7;
+const int MinusSwitch = 7;
 int valMinusSwitch = 0;
 int lastMinusSwitch = 0;
 
-int PlusSwitch = 22;
+const int PlusSwitch = 22;
 int valPlusSwitch = 0;
 int lastPlusSwitch = 0;
 
-int TempoSwitch = 23;
+const int TempoSwitch = 23;
 int valTempoSwitch = 0;
 int lastTempoSwitch = 0;
 
-int StopSwitch = 24;
+const int StopSwitch = 24;
 int valStopSwitch = 0;
 int lastStopSwitch = 0;
 
-int PlaySwitch = 25;
+const int PlaySwitch = 25;
 int valPlaySwitch = 0;
 int lastPlaySwitch = 0;
 
-int RecSwitch = 26;
+const int RecSwitch = 26;
 int valRecSwitch = 0;
 int lastRecSwitch = 0;
 
-int BankPad = 27;
+const int BankPad = 27;
 int valBankPad = 0;
 int lastBankPad = 0;
 
-int CrashPad = 28;
+const int CrashPad = 28;
 int valCrashPad = 0;
 int lastCrashPad = 0;
 
-int CupPad = 29;
+const int CupPad = 29;
 int valCupPad = 0;
 int lastCupPad = 0;
 
-int RidePad = 30;
+const int RidePad = 30;
 int valRidePad = 0;
 int lastRidePad = 0;
 
-int FunctionPad = 31;
+const int FunctionPad = 31;
 int valFunctionPad = 0;
 int lastFunctionPad = 0;
 
-int KickPad = 32;
+const int KickPad = 32;
 int valKickPad = 0;
 int lastKickPad = 0;
 
-int SnarePad = 33;
+const int SnarePad = 33;
 int valSnarePad = 0;
 int lastSnarePad = 0;
 
-int ClosedHatPad = 34;
+const int ClosedHatPad = 34;
 int valClosedHatPad = 0;
 int lastClosedHatPad = 0;
 
-int OpenHatPad = 35;
+const int OpenHatPad = 35;
 int valOpenHatPad = 0;
 int lastOpenHatPad = 0;
