a4859394fb89a3547a91f6ec3142fe2642360018
HyDz <jean@toutourien.org>
2017-03-13 17:37:02 +0100

    Update outputs.h

--- main/outputs.h
+++ main/outputs.h
@@ -7,20 +7,20 @@
  * Declare Digitals Outputs
  */ 
  
- int SongLed = 36;
- int PatternLed = 37;
- int KitLed = 38;
- int MinusLed = 39;
- int PlusLed = 40;
- int TempoLed = 41;
- int PlayLed = 42;
- int RecLed = 43;
- int BankLed = 44;
- int CrashLed = 45;
- int CupLed = 46;
- int RideLed = 47;
- int FunctionLed = 48;
- int KickLed = 49;
- int SnareLed = 50;
- int ClosedHatLed = 51;
- int OpenHatLed = 52;
+ const int SongLed = 36;
+ const int PatternLed = 37;
+ const int KitLed = 38;
+ const int MinusLed = 39;
+ const int PlusLed = 40;
+ const int TempoLed = 41;
+ const int PlayLed = 42;
+ const int RecLed = 43;
+ const int BankLed = 44;
+ const int CrashLed = 45;
+ const int CupLed = 46;
+ const int RideLed = 47;
+ const int FunctionLed = 48;
+ const int KickLed = 49;
+ const int SnareLed = 50;
+ const int ClosedHatLed = 51;
+ const int OpenHatLed = 52;
