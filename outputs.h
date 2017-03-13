0b8e519d96a46b92abd132fd9f0dc4f1c5eaa8cb
HyDz <jean@toutourien.org>
2017-03-13 16:30:09 +0100

    Create outputs.h

---
+++ outputs.h
@@ -0,0 +1,26 @@
+/*
+ * ZoomMRT-3B_MIDI_USB
+ *
+ * Created: 13/3/2017 
+ * Author: HyDz
+ * USB Midified ZoomMRT-3B for Traktor
+ * Declare Digitals Outputs
+ */ 
+ 
+ int SongLed = 36;
+ int PatternLed = 37;
+ int KitLed = 38;
+ int MinusLed = 39;
+ int PlusLed = 40;
+ int TempoLed = 41;
+ int PlayLed = 42;
+ int RecLed = 43;
+ int BankLed = 44;
+ int CrashLed = 45;
+ int CupLed = 46;
+ int RideLed = 47;
+ int FunctionLed = 48;
+ int KickLed = 49;
+ int SnareLed = 50;
+ int ClosedHatLed = 51;
+ int OpenHatLed = 52;
