4bbbf96de2b7e3ea640ddebec8e0144f43306707
HyDz <jean@toutourien.org>
2017-03-14 00:53:53 +0100

    Update inputs.h

--- main/inputs.h
+++ main/inputs.h
@@ -22,7 +22,8 @@
 #define COUNTER_CLOCKWISE    2       // direction constant
  
 int JogWheelActive = 0;
-int JogWheelMaxval = 200;
+int JogWheelMaxval = 4000;
+int JogWheelToggle = 200; // To prevent intempestive move
 // variables modified by interrupt handler must be declared as volatile
 volatile long valJogWheel = 0;
 volatile long mappedvalJogWheel = 0;
@@ -73,54 +74,80 @@ int lastMinusSwitch = 0;
 int valPlusSwitch = 0;
 int lastPlusSwitch = 0;
 
+
 #define TempoSwitch  23
+
 int valTempoSwitch = 0;
 int lastTempoSwitch = 0;
 
-const int StopSwitch = 24;
+
+#define StopSwitch  24
+
 int valStopSwitch = 0;
 int lastStopSwitch = 0;
 
-const int PlaySwitch = 25;
+
+#define PlaySwitch  25
+
 int valPlaySwitch = 0;
 int lastPlaySwitch = 0;
 
-const int RecSwitch = 26;
+
+#define RecSwitch  26
+
 int valRecSwitch = 0;
 int lastRecSwitch = 0;
 
-const int BankPad = 27;
+
+#define BankPad  27
+
 int valBankPad = 0;
 int lastBankPad = 0;
 
-const int CrashPad = 28;
+
+#define CrashPad  28
+
 int valCrashPad = 0;
 int lastCrashPad = 0;
 
-const int CupPad = 29;
+
+#define CupPad  29
+
 int valCupPad = 0;
 int lastCupPad = 0;
 
-const int RidePad = 30;
+
+#define RidePad  30
+
 int valRidePad = 0;
 int lastRidePad = 0;
 
-const int FunctionPad = 31;
+
+#define FunctionPad  31
+
 int valFunctionPad = 0;
 int lastFunctionPad = 0;
 
-const int KickPad = 32;
+
+#define KickPad  32
+
 int valKickPad = 0;
 int lastKickPad = 0;
 
-const int SnarePad = 33;
+
+#define SnarePad  33
+
 int valSnarePad = 0;
 int lastSnarePad = 0;
 
-const int ClosedHatPad = 34;
+
+#define ClosedHatPad  34
+
 int valClosedHatPad = 0;
 int lastClosedHatPad = 0;
 
-const int OpenHatPad = 35;
+
+#define OpenHatPad  35
+
 int valOpenHatPad = 0;
 int lastOpenHatPad = 0;
