acfed846877156700721b5768dcc002608421760
HyDz <jean@toutourien.org>
2017-03-14 03:04:39 +0100

    Update outputs.h

--- main/outputs.h
+++ main/outputs.h
@@ -6,21 +6,23 @@
  * USB Midified ZoomMRT-3B for Traktor
  * Declare Digitals Outputs
  */ 
+
+#define NUM_LEDS    17  // Number of LEDS
+
+char* outputsNames[NUM_LEDS] = { "SongLed", "PatternLed", "KitLed", "MinusLed", "PlusLed", "TempoLed",  "PlayLed", "RecLed", "BankLed", "CrashLed", "CupLed", "RideLed", "FunctionLed", "KickLed", "SnareLed", "ClosedHatLed", "OpenHatLed" }; 
+const byte outputPins[NUM_LEDS] ={ 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52}; //Declare Outputs Pins in the same order than names
+
+void inputs.begin(){
  
- const int SongLed = 36;
- const int PatternLed = 37;
- const int KitLed = 38;
- const int MinusLed = 39;
- const int PlusLed = 40;
- const int TempoLed = 41;
- const int PlayLed = 42;
- const int RecLed = 43;
- const int BankLed = 44;
- const int CrashLed = 45;
- const int CupLed = 46;
- const int RideLed = 47;
- const int FunctionLed = 48;
- const int KickLed = 49;
- const int SnareLed = 50;
- const int ClosedHatLed = 51;
- const int OpenHatLed = 52;
+ for (int i = 0; i < NUM_LEDS; i++) {
+ #define outputsNames[i]  outputPins[i]; // Declare Name and Pins
+ pinMode(outputPins[i], OUTPUT); //Set Outputs
+ digitalWrite(i, HIGH);
+ }
+ 
+ for (int i = 0; i < NUM_LEDS; i++) {
+ digitalWrite(i, LOW);
+ }
+ 
+}
+
