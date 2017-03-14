efa5f3cb561023a96a203dcaef3a124ce924a780
HyDz <jean@toutourien.org>
2017-03-14 16:05:42 +0100

    Update outputs.h

--- main/outputs.h
+++ main/outputs.h
@@ -12,7 +12,7 @@
 char* outputsNames[NUM_LEDS] = { "SongLed", "PatternLed", "KitLed", "MinusLed", "PlusLed", "TempoLed",  "PlayLed", "RecLed", "BankLed", "CrashLed", "CupLed", "RideLed", "FunctionLed", "KickLed", "SnareLed", "ClosedHatLed", "OpenHatLed" }; 
 const byte outputPins[NUM_LEDS] ={ 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52}; //Declare Outputs Pins in the same order than names
 
-void inputs.begin(){
+void outputsinit(){
  
  for (int i = 0; i < NUM_LEDS; i++) {
  #define outputsNames[i]  outputPins[i]; // Declare Name and Pins
