254fd6ad79aabbfa187a8063907c93ed31d8180c
HyDz <jean@toutourien.org>
2017-03-20 14:43:49 +0100

    Update inputs.h

--- main/inputs.h
+++ main/inputs.h
@@ -156,7 +156,7 @@ int lastClosedHatPad = 0;
 unsigned long lastClosedHatPadDebounceTime = 0;
 int ClosedHatPadState = 0;
 
-int OpenHatPad = 35;
+int OpenHatPad = 43;
 int valOpenHatPad = 0;
 int lastOpenHatPad = 0;
 unsigned long lastOpenHatPadDebounceTime = 0;
@@ -165,7 +165,7 @@ int OpenHatPadState = 0;
 
 
 char* inputsNames[NUM_BUTTONS] = { "SongSwitch", "PatternSwitch", "KitSwitch", "InsertSwitch", "DeleteSwitch", "MinusSwitch", "PlusSwitch", "TempoSwitch", "StopSwitch", "PlaySwitch", "RecSwitch", "BankPad", "CrashPad", "CupPad", "RidePad", "FunctionPad", "KickPad", "SnarePad", "ClosedHatPad", "OpenHatPad" };
-const byte inputPins[NUM_BUTTONS] = { 2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35}; //Declare Inputs Pins in the same order than names
+const byte inputPins[NUM_BUTTONS] = { 2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 43}; //Declare Inputs Pins in the same order than names
 
 
 void inputsinit() {
